c3fb7bfca48008a49d06add7544ed9699e336661
yanruijie902136 <yanruijie902136@gmail.com>
2025-05-11 15:46:32 +0800

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,14 +1,47 @@
+#include <limits.h>
 #include <readline/readline.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char *argv[]) {
+#ifndef ARG_MAX
+#define ARG_MAX 4096
+#endif
+
+static int argc;
+static char *argv[ARG_MAX];
+
+static void parse_line(char *line) {
+    argc = 0;
+    char *arg = strtok(line, " ");
+    while (arg != NULL) {
+        argv[argc++] = arg;
+        arg = strtok(NULL, " ");
+    }
+    argv[argc] = NULL;
+}
+
+static void cmd_exit(void) {
+    int status = atoi(argv[1]);
+    exit(status);
+}
+
+int main(void) {
     for ( ; ; ) {
         char *line = readline("$ ");
-        if (line == NULL) {
+        if (line == NULL) {     // End of input.
             exit(EXIT_SUCCESS);
         }
-        printf("%s: command not found\n", line);
+
+        parse_line(line);
+
+        const char *cmd = argv[0];
+        if (strcmp(cmd, "exit") == 0) {
+            cmd_exit();
+        } else {
+            fprintf(stderr, "%s: command not found\n", cmd);
+        }
+
         free(line);
     }
 }
