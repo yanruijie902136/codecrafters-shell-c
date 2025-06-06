bfe17e572a068cf0eb5613260d7c89dac8cf2b12
yanruijie902136 <yanruijie902136@gmail.com>
2025-06-06 15:58:39 +0800

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -9,6 +9,13 @@
 #include "scan.h"
 #include "token.h"
 
+static void write_history_file(void) {
+    const char *histfile = getenv("HISTFILE");
+    if (histfile != NULL) {
+        write_history(histfile);
+    }
+}
+
 static void setup(void) {
     rl_attempted_completion_function = shell_completion;
 
@@ -17,6 +24,7 @@ static void setup(void) {
     if (histfile != NULL) {
         read_history(histfile);
     }
+    atexit(write_history_file);
 }
 
 static PtrArray *parse_line_to_cmds(const char *line) {
